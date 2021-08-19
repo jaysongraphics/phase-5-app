5225378ed6f7e8677241dbe1b7176cc02f43b5d2
Edward <jaysongraphics@gmail.com>
2021-08-19 18:44:38 -0400

    draft

--- App.js
+++ App.js
@@ -1,64 +1,200 @@
 import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { Stylesheet,Button, Text, View, ImageBackground } from 'react-native';
+import { Stylesheet, Button, Text, View, ImageBackground, TouchableOpacity } from 'react-native';
 import style from './components/style'
 import Quotes from './components/quotes'
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {useState, useEffect} from 'react'
+import logo from './images/BRAINONLYBLACK.png'
+import profile from './images/profile.jpg'
+import profilee from './images/aaaa.png'
+import itore from './images/store.jpg'
 
 function App() {
+// const Stack = createNativeStackNavigator();
+// const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
-const Stack = createNativeStackNavigator();
+
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: '' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Get motivated"
-        onPress={() => navigation.navigate('Quotes')}/>
+    <View 
+    style={{ flex: 1, alignItems: 'center', justifyContent: '' }}
+    >
+    <Text style={style.homelogo}>Welcome!</Text>
+      <ImageBackground source={logo} style={style.logooig}/>
     </View>
   );
 }
 
-
-function DetailsScreen({ navigation }) {
+function DetailQuotes({ navigation }) {
   return (
   <View 
   style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
   >
-    <Text style={style.logo}>Selflax</Text>
+    <Text style={style.homelogoa}>Quotes</Text>
         <View style={style.homePage}>  
         <Text style={style.text}>Get motivated!</Text> 
-          <Quotes />
-      <Button 
-      // style={style.buttons} 
-      title="Go to Home" 
-      onPress={() => navigation.navigate('Home')} />
-      <Button title="Go back" onPress={() => navigation.goBack()} />
-    </View>
+          <Quotes /> 
+       </View>
     </View>
   );
 }
 
-    return (
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
-
-              <Stack.Screen
-                name="Home"
-                component={HomeScreen}
-                options={{ title: 'Home' }}
-              />
-
-              <Stack.Screen
-                name="Quotes"
-                component={DetailsScreen}
-                options={{ title: 'Quotes' }}
-              />
-          </Stack.Navigator>
-        </NavigationContainer>
-    );
+function Profile({ navigation }) {
+  return (
+    <View 
+    style={{ flex: 1, alignItems: 'center', justifyContent: '' }}
+    >
+       <Text style={style.profilepri}>Profile</Text>
+        <ImageBackground
+        source={profilee} style={style.lopri}/>
+      {/* <Button
+        title="Get motivated"
+        onPress={() => navigation.navigate('Quotes')}/> */}
+        </View>
+  );
 }
+
+function About({ navigation }) {
+  return (
+    <View 
+    style={{ flex: 1, alignItems: 'center', justifyContent: '' }}
+    >
+  <Text style={style.homelogo}>About</Text>
+    <Text style={style.para}> Breaking Taboo
+      Our intention is to educate, share, and encourage open conversations about this topic. We believe that in order to solve a problem, we must focus on the root cause. In order to save lives, we must kill the silence. In order to kill the silence we must break the taboo.</Text> 
+
+   <View>
+    <Text style={style.parasecond}>Our Services</Text>
+    </View>
+     <View> 
+    <Text style={style.parathrid}>✔️Book appointments</Text>
+   <Text style={style.parathrid}>✔️Book an appointment with your favorite therapists!</Text>
+   <Text style={style.parathrid}>✔️Review therapists</Text>
+    <Text style={style.parathrid}>✔️Review your therapists, tell the community how good they are!</Text>
+      <Text style={style.parathrid}>✔️Share your feelings</Text>
+      <Text style={style.parathrid}>✔️Do not hide your feelings, share them.</Text>
+        <Text style={style.parathrid}>✔️Chat with friends</Text>
+       <Text style={style.parathrid}>✔️Talk to your friends online.</Text>
+       </View> 
+  
+    </View>
+  );
+}
+
+
+function Store({ navigation }) {
+  return (
+    <View 
+    style={{ flex: 1, alignItems: 'center', justifyContent: '' }}
+    >
+       <Text style={style.profilepri}>Store</Text>
+        <ImageBackground
+        source={itore} style={style.lopri}/>
+      {/* <Button
+        title="Get motivated"
+        onPress={() => navigation.navigate('Quotes')}/> */}
+        </View>
+  );
+}
+
+    return (
+      <NavigationContainer>
+      <Tab.Navigator
+          initialRouteName="Home"
+          activeColor="#e91e63"
+          barStyle={{ backgroundColor: 'black' }}>
+            <Tab.Screen
+              name="Home"
+              component={HomeScreen}
+              options={{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="home" color={color} size={26} />
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="Quotes"
+              component={DetailQuotes}
+              options={{
+                tabBarLabel: 'Quotes',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="library" color={color} size={26} />
+                ),
+              }}
+            />
+
+         <Tab.Screen
+              name="Profile"
+              component={Profile}
+              options={{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="account" color={color} size={26} />
+                ),
+              }}
+            /> 
+
+        <Tab.Screen
+              name="About"
+              component={About}
+              options={{
+                tabBarLabel: 'About',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="details" color={color} size={26} />
+                ),
+              }}
+          /> 
+
+          <Tab.Screen
+              name="Store"
+              component={Store}
+              options={{
+                tabBarLabel: 'Store',
+                tabBarIcon: ({ color }) => (
+                  <MaterialCommunityIcons name="settings" color={color} size={26} />
+                ),
+              }}
+          /> 
+
+         </Tab.Navigator>
+          </NavigationContainer>
+       );
+      } 
+      
+    //   <NavigationContainer>
+    //   <Tab.Navigator>
+    //   <Tab.Screen name="Home" component={HomeScreen} />
+    //   <Tab.Screen name="Quotes" component={DetailQuotes} />
+    //   </Tab.Navigator>
+    //   </NavigationContainer>
+    // )}
+
+        // <NavigationContainer>
+        //   <Stack.Navigator initialRouteName="Home">
+
+        //       <Stack.Screen
+        //         name="Home"
+        //         component={HomeScreen}
+        //         options={{ title: 'Home' }}
+        //       />
+
+        //       <Stack.Screen
+        //         name="Quotes"
+        //         component={DetailQuotes}
+        //         options={{ title: 'Quotes' }}
+        //       />
+        //   </Stack.Navigator>
+        // </NavigationContainer>
+//     );
+// }
 
 
 
@@ -95,3 +231,46 @@ function DetailsScreen({ navigation }) {
 
 
 // export default App;
+
+
+
+{/* <NavigationContainer>
+<Tab.Navigator
+      initialRouteName="Feed"
+      activeColor="#e91e63"
+      barStyle={{ backgroundColor: 'tomato' }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={DetailQuotes}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Notifications"
+        component={DetailQuotes}
+        options={{
+          tabBarLabel: 'Updates',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={26} />
+          ),
+        }}
+      />
+   <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="account" color={color} size={26} />
+          ),
+        }}
+      /> 
+   </Tab.Navigator>
+    </NavigationContainer>
+ );
+}  */}
--- components/style.jsx
+++ components/style.jsx
@@ -10,23 +10,100 @@ export default style = StyleSheet.create({
     },
     homePage:{
       width: '100%',
-      height: '100%'
+      height: '100%',
+      backgroundColor: 'black' 
     },
-  
-    logo: {
-      fontSize: 30,
-      marginTop: '10%',
+
+    homehomePage:{
+    },
+
+    homelogo:{
+
+      marginTop: '30%',
+      fontSize: 90
+    },
+
+    homelogoa: {
+      fontSize: 80,
+      marginTop: '50%',
+      // color: 'black'
     },
 
     text: {
       fontSize: 15,
       marginTop: '0%',
       marginLeft: '38%',
+      color: 'white'
     },
     
+    hometext:{
+    },
+
+    para:{
+      fontSize: 25,
+      marginTop: '8%',
+    },
+
+    parasecond:{
+      fontSize: 20,
+      marginTop: '2%',
+    },
+
+    logoo:{
+      width: '100%',
+      height: '100%',
+    },
+
+    logooo:{
+      width: '5%',
+      height: '5%',
+    },
+
+    homelogoaa:{
+      width: '5%',
+      height: '5%',
+    },
+
+    logooig:{
+      width: '100%',
+      height: '85%',
+    },
+
+    lopri:{
+      width: '100%',
+      height: '85%',
+    },
+
+    lopri:{
+      // marginTop: '30%',
+      width: '100%',
+      height: '100%',
+      // marginBottom: '90%',
+    },
+    
+    profilepri:{
+    marginTop: '10%',
+    fontSize: 40,
+    },
+    // hompro:{
+    //   marginTop: '40%'
+    // },
+
+    parathrid:{
+      fontSize: 18,
+      marginTop: '2%',
+      marginLeft: '2%',
+      marginRight: '%',
+    },
+    // hometext: {
+    //   color: 'black'
+    // },
+
     quote: {
       fontSize: 50,
-      marginTop: '10%',
+      marginTop: '30%',
+      marginBottom: '45%',
+      color: 'white'
     }, 
   
     // buttons: {
@@ -41,6 +118,7 @@ export default style = StyleSheet.create({
       width: '100%',
       alignItems: 'center',
       height: '',
+      
     }
   });
   
